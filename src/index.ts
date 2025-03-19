3f3316997b29641c713e98496c09924e0fe57a00
Bitthal Verma <bitthal@Bitthals-MacBook-Air.local>
2025-03-19 17:04:48 +0530

    added helmet

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import router from './routes/index';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,7 @@ const app = express();
 
 
 app.use(cors());
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
